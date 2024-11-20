314948025a2012f708be89cf2e32562380613612
washerd <washerd2008@gmail.com>
2024-11-20 21:22:50 +0100

    Implement tests

--- tests/csv-search/csv-search.spec.ts
+++ tests/csv-search/csv-search.spec.ts
@@ -1,11 +1,121 @@
 import assert from 'node:assert/strict';
-import { describe, it } from 'node:test';
+import { describe, it, before, after } from 'node:test';
 import { generateCsv } from '../utils/generate-csv';
+import { CsvSearch, outputFormats } from '../../packages/csv-search/csv-search';
+
+const csvFileName = 'test.csv';
 
 describe('CSV Search', () => {
+    let cleanupCsv: () => void;
+    let csvSnap: Record<string, string>[];
+    before(async () => {
+        const { cleanup, jsObjectSnapshot } = await generateCsv(csvFileName);
+        cleanupCsv = cleanup;
+        csvSnap = jsObjectSnapshot;
+    });
+    after(() => {
+        cleanupCsv();
+    });
+
+    it('should support TemplateStringsArray call', async () => {
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+
+        const result = searcher.query`PROJECT col1, col2 FILTER col3 = ${value}`;
+        assert.ok(result);
+    });
+    it('should support string call', async () => {
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+
+        const result = searcher.query(`PROJECT col1, col2 FILTER col3 = ${value}`);
+        assert.ok(result);
+    });
     it('should find a record by a column value', async () => {
-        const cleanup = await generateCsv();
-        cleanup();
-        assert.equal(0, 1);
-    })
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+        const dataIndex = csvSnap.findIndex((record) => Number(record.Col3) === value);
+
+        const result = searcher.query`PROJECT col1, col2 FILTER col3 = ${value}`;
+
+        const expected = [{ col1: csvSnap[dataIndex].Col1, col2: csvSnap[dataIndex].Col2 }];
+        assert.deepEqual(result, expected);
+    });
+    it('should find records by gt operator', async () => {
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+
+        const result = searcher.query`PROJECT col1, col2 FILTER col3 > ${value}`;
+
+        // see generateCsv method for the data
+        const expected = [
+            { col1: '99000', col2: 'pqr678' },
+            { col1: '11122', col2: '123' },
+            { col1: '33344', col2: 'vwx234' },
+            { col1: '77788', col2: 'BCD890' }];
+        assert.deepEqual(result, expected);
+    });
+    it('should find records by gte operator', async () => {
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+
+        const result = searcher.query`PROJECT col1, col2 FILTER col3 >= ${value}`;
+
+        // see generateCsv method for the data
+        const expected = [
+            { col1: '77889', col2: 'mno345' },
+            { col1: '99000', col2: 'pqr678' },
+            { col1: '11122', col2: '123' },
+            { col1: '33344', col2: 'vwx234' },
+            { col1: '77788', col2: 'BCD890' },
+        ];
+        assert.deepEqual(result, expected);
+    });
+    it('should find records by lt operator', async () => {
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+
+        const result = searcher.query`PROJECT col1, col2 FILTER col3 < ${value}`;
+
+        // see generateCsv method for the data
+        const expected = [
+            { col1: '12345', col2: 'abc123' },
+            { col1: '55566', col2: 'yzA567' },
+            { col1: '67890', col2: 'def456' },
+            { col1: '11223', col2: 'ghi789' },
+            { col1: '44556', col2: 'jkl012' },
+        ];
+        assert.deepEqual(result, expected);
+    });
+    it('should find records by lte operator', async () => {
+        const searcher = new CsvSearch(outputFormats.table);
+        await searcher.loadCsv(csvFileName);
+
+        const value = 33;
+
+        const result = searcher.query`PROJECT col1, col2 FILTER col3 <= ${value}`;
+
+        // see generateCsv method for the data
+        const expected = [
+            { col1: '12345', col2: 'abc123' },
+            { col1: '55566', col2: 'yzA567' },
+            { col1: '67890', col2: 'def456' },
+            { col1: '11223', col2: 'ghi789' },
+            { col1: '44556', col2: 'jkl012' },
+            { col1: '77889', col2: 'mno345' },
+        ];
+        assert.deepEqual(result, expected);
+    });
 });
--- tests/utils/generate-csv.ts
+++ tests/utils/generate-csv.ts
@@ -15,43 +15,159 @@ type Record = {
 };
 
 /**
- * Generate a CSV file with 1000 rows and 10 columns
+ * Generate a CSV file with 10 rows and 10 columns
  * first column contains a string of 5-15 numbers in Unicode format
  * second column contains a string of 5-15 letters and numbers in Unicode format
  * other columns contain numbers
  * @param path
+ *
+ * static snapshot:
+ * | Col1  | Col2   | Col3 | Col4 | Col5 | Col6 | Col7 | Col8 | Col9 | Col10 |
+ * |-------|--------|------|------|------|------|------|------|------|-------|
+ * | 12345 | abc123 | 1    | 2    | 3    | 4    | 5    | 6    | 7    | 8     |
+ * | 67890 | def456 | 9    | 10   | 11   | 12   | 13   | 14   | 15   | 16    |
+ * | 11223 | ghi789 | 17   | 18   | 19   | 20   | 21   | 22   | 23   | 24    |
+ * | 44556 | jkl012 | 25   | 26   | 27   | 28   | 29   | 30   | 31   | 32    |
+ * | 77889 | mno345 | 33   | 34   | 35   | 36   | 37   | 38   | 39   | 40    |
+ * | 99000 | pqr678 | 41   | 42   | 43   | 44   | 45   | 46   | 47   | 48    |
+ * | 11122 | 123    | 49   | 50   | 51   | 52   | 53   | 54   | 55   | 56    |
+ * | 33344 | vwx234 | 57   | 58   | 59   | 60   | 61   | 62   | 63   | 64    |
+ * | 55566 | yzA567 | 3   | 66   | 67   | 68   | 69   | 70   | 71   | 72    |
+ * | 77788 | BCD890 | 73   | 74   | 75   | 76   | 77   | 78   | 79   | 80    |
  */
 export async function generateCsv(path = 'generated_data.csv') {
+    const header = Array.from({ length: 10 }, (_, i) => ({
+        id: `Col${i + 1}`,
+        title: `Col${i + 1}`,
+    }));
+
     const csvWriter = createObjectCsvWriter({
         path: path,
-        header: [
-            { id: 'Col1', title: 'Col1' },
-            { id: 'Col2', title: 'Col2' },
-            { id: 'Col3', title: 'Col3' },
-            { id: 'Col4', title: 'Col4' },
-            { id: 'Col5', title: 'Col5' },
-            { id: 'Col6', title: 'Col6' },
-            { id: 'Col7', title: 'Col7' },
-            { id: 'Col8', title: 'Col8' },
-            { id: 'Col9', title: 'Col9' },
-            { id: 'Col10', title: 'Col10' },
-        ],
+        header: header,
     });
 
-
-    const records: Record[] = [];
-    for (let i = 0; i < 10; i++) {
-        const record: Record = {} as Record;
-        record['Col1'] = unicodeNumberString(Math.floor(Math.random() * 10) + 5);
-        record['Col2'] = mixedUnicodeString(Math.floor(Math.random() * 10) + 5);
-
-        for (let j = 3; j <= 10; j++) {
-            const key = `Col${j}` as keyof Record;
-            record[key] = unicodeNumberString(Math.floor(Math.random() * 20) + 1);
-        }
-        records.push(record);
-    }
-
+    const records: Record[] = [
+        {
+            Col1: '12345',
+            Col2: 'abc123',
+            Col3: '1',
+            Col4: '2',
+            Col5: '3',
+            Col6: '4',
+            Col7: '5',
+            Col8: '6',
+            Col9: '7',
+            Col10: '8',
+        },
+        {
+            Col1: '67890',
+            Col2: 'def456',
+            Col3: '9',
+            Col4: '10',
+            Col5: '11',
+            Col6: '12',
+            Col7: '13',
+            Col8: '14',
+            Col9: '15',
+            Col10: '16',
+        },
+        {
+            Col1: '11223',
+            Col2: 'ghi789',
+            Col3: '17',
+            Col4: '18',
+            Col5: '19',
+            Col6: '20',
+            Col7: '21',
+            Col8: '22',
+            Col9: '23',
+            Col10: '24',
+        },
+        {
+            Col1: '44556',
+            Col2: 'jkl012',
+            Col3: '25',
+            Col4: '26',
+            Col5: '27',
+            Col6: '28',
+            Col7: '29',
+            Col8: '30',
+            Col9: '31',
+            Col10: '32',
+        },
+        {
+            Col1: '77889',
+            Col2: 'mno345',
+            Col3: '33',
+            Col4: '34',
+            Col5: '35',
+            Col6: '36',
+            Col7: '37',
+            Col8: '38',
+            Col9: '39',
+            Col10: '40',
+        },
+        {
+            Col1: '99000',
+            Col2: 'pqr678',
+            Col3: '41',
+            Col4: '42',
+            Col5: '43',
+            Col6: '44',
+            Col7: '45',
+            Col8: '46',
+            Col9: '47',
+            Col10: '48',
+        },
+        {
+            Col1: '11122',
+            Col2: '123',
+            Col3: '49',
+            Col4: '50',
+            Col5: '51',
+            Col6: '52',
+            Col7: '53',
+            Col8: '54',
+            Col9: '55',
+            Col10: '56',
+        },
+        {
+            Col1: '33344',
+            Col2: 'vwx234',
+            Col3: '57',
+            Col4: '58',
+            Col5: '59',
+            Col6: '60',
+            Col7: '61',
+            Col8: '62',
+            Col9: '63',
+            Col10: '64',
+        },
+        {
+            Col1: '55566',
+            Col2: 'yzA567',
+            Col3: '3',
+            Col4: '66',
+            Col5: '67',
+            Col6: '68',
+            Col7: '69',
+            Col8: '70',
+            Col9: '71',
+            Col10: '72',
+        },
+        {
+            Col1: '77788',
+            Col2: 'BCD890',
+            Col3: '73',
+            Col4: '74',
+            Col5: '75',
+            Col6: '76',
+            Col7: '77',
+            Col8: '78',
+            Col9: '79',
+            Col10: '80',
+        },
+    ];
 
     try {
         await csvWriter.writeRecords(records);
@@ -60,7 +176,10 @@ export async function generateCsv(path = 'generated_data.csv') {
         console.error('Failed to write CSV file', error);
     }
 
-    return () => removeCsv(path);
+    return {
+        cleanup: () => removeCsv(path),
+        jsObjectSnapshot: records,
+    };
 }
 
 function removeCsv(path = 'generated_data.csv') {
@@ -70,31 +189,4 @@ function removeCsv(path = 'generated_data.csv') {
     } catch (error) {
         console.error('Failed to remove CSV file', error);
     }
-}
-
-/**
- * Generate a string with only numbers in Unicode format
- * @param length
- */
-function unicodeNumberString(length: number): string {
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += String.fromCharCode(48 + Math.floor(Math.random() * 10));
-    }
-    return result;
-}
-
-/**
- * Generate a string with a mix of letters and numbers in Unicode format
- * @param length
- */
-function mixedUnicodeString(length: number): string {
-    let result = '';
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    for (let i = 0; i < length; i++) {
-        result += Math.random() > 0.5
-            ? String.fromCharCode(48 + Math.floor(Math.random() * 10))
-            : chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
 }
